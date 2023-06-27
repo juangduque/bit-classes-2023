ffe3287f024bc0abac408d3cf7582d4b0f9a2f45
Juan Duque <juangduqued@gmail.com>
2023-06-27 09:43:55 -0500

    add index.ts main file changes

--- class-11-express-part1/index.ts
+++ class-11-express-part1/index.ts
@@ -1,11 +1,12 @@
 // const express = require('express'); // Esto es un enfoque viejo para importar módulos de js.
-import express from 'express';
+import express from 'express'; // Esta es la forma de importar módulos de js en typescript. Es una buena práctica que el nombre de la constante sea igual al nombre del módulo que se está importando.
 
 const app = express(); // Esta es la creación de la instancia.
-const PORT = 3000;
+const PORT = 3000; // Se declara una variable para el puerto. Es una buena práctica que sea una constante.
 
 app.use(express.json()); //Esto es un middleware. En este caso es necesario para poder user el body que existe dentro de la petición http.
 
+// Se crea una interfaz para definir el tipo de dato que va a tener el arreglo customers.
 interface Customer {
     id: string;
     name: string;
@@ -40,13 +41,13 @@ let customersDB: Customer[] = [
 
 app.get('/customers', function (request, response) {
     // response.send('Hola mundo desde get'); // La diferencia entre response.send y response.json es que el primero solo retorna cadenas de caracteres (strings) y el segundo json
-    response.json(customersDB);
+    response.json(customersDB); // Se retorna el arreglo customersDB
 });
 
 app.get('/customers/:id', function (request, response) {
-    const id = request.params.id;
-    const result = customersDB.filter(item => item.id === id);
-    response.json(result);
+    const id = request.params.id; // Esta es la forma de obtener el parámetro id que viene en la url
+    const result = customersDB.filter(item => item.id === id); // Esta es la forma de filtrar un arreglo. En este caso se filtra por el id. Aquel item que cumpla con la condición se va a quedar en el arreglo result.
+    response.json(result); // Se retorna el resultado de la operación anterior.
 });
 
 /*
@@ -55,31 +56,33 @@ app.get('/customers/:id', function (request, response) {
     customers o su representación es que "se guarda información en la base de datos".
 */
 app.post('/customers', function (request, response) {
-    const body = request.body;
-    customersDB.push(body);
-    response.send('El cliente se ha guardado');
+    const body = request.body; // Esta es la forma de obtener el body de la petición http
+    customersDB.push(body); // Se hace push al arreglo customersDB
+    response.send('El cliente se ha guardado'); // Se retorna un mensaje
 });
 
+
+// El método put es similar al post, pero en este caso se actualiza la información.
 app.put('/customers/:id', function (request, response) {
     // El id y el body para la operación put son requeridas
-    const id = request.params.id;
-    const body = request.body;
-    const customerIndex = customersDB.findIndex(item => item.id === id);
-    console.log("customerIndex", customerIndex);
-    customersDB[customerIndex] = body;
-    response.send('Cliente actualizado correctamente');
+    const id = request.params.id; // Esta es la forma de obtener el parámetro id que viene en la url
+    const body = request.body; // Esta es la forma de obtener el body de la petición http
+    const customerIndex = customersDB.findIndex(item => item.id === id); // Esta es la forma de obtener el índice del elemento que se quiere actualizar.
+    customersDB[customerIndex] = body; // Se actualiza la información en el arreglo customersDB
+    response.send('Cliente actualizado correctamente'); // Se retorna un mensaje
 });
 
+// El método delete es similar al put, pero en este caso se elimina la información.
 app.delete('/customers/:id', function (request, response) {
-    const id = request.params.id;
-    const result = customersDB.filter(item => item.id !== id);
-    customersDB = result;
-    response.json("Cliente eliminado correctamente");
+    const id = request.params.id; // Esta es la forma de obtener el parámetro id que viene en la url
+    const result = customersDB.filter(item => item.id !== id); // Esta es la forma de filtrar un arreglo. En este caso se filtra por el id. Aquel item que cumpla con la condición se va a quedar en el arreglo result.
+    customersDB = result; // Se actualiza la información en el arreglo customersDB
+    response.json("Cliente eliminado correctamente"); // Se retorna un mensaje
 });
 
 // Esta es la sintaxis para subir/activar el servidor
 // app.listen( Puerto_Donde_Escucha, Callback_opcional );
 // La funcionalidad del callback es ejecutar ese código la primera vez que se levanta el servidor
 app.listen(PORT, function () {
-    console.log("La aplicación es está ejecutando en: https//localhost:" + PORT);
+    console.log("La aplicación es está ejecutando en: http://localhost:" + PORT); // Se imprime en consola la confirmación de que el servidor está activo
 });
