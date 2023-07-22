20be98e689767af67b739f8d4d1fc4bb06c987c6
Juan Duque <juangduqued@gmail.com>
2023-07-21 20:15:29 -0500

    add angular directives content

--- class-16-angular-data-handle/angular-data/src/app/app.component.ts
+++ class-16-angular-data-handle/angular-data/src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core'; // Se importa el decorador de angular que extiende el comportamiento propio de un componente de angular.
 
+interface Product {
+  id: string,
+  name: string,
+  price: number,
+  description: string,
+  image: string,
+}
+
 // Se crea un decorador para el componente, el cual recibe un objeto con las propiedades del componente: selector, templateUrl, styleUrls.
 @Component({
   selector: 'app-root', // Se define el selector del componente, el cual se utiliza para llamar al componente en el html.
@@ -18,17 +26,83 @@ export class AppComponent {
 
   fruits: string[] = ["apple", "kiwi", "banana"]
 
-  type: string = "button"
+  type: string = "date"
   disabled: boolean = false;
   imgUrl: string = "https://tse1.mm.bing.net/th?id=OIP.0e365QXKL-NORBeVdozttgHaHe&pid=Api&P=0&h=180";
 
   variableModificable: string = "valor";
 
   dataPerson = {
-    name: "",
+    name: "nicolas",
     lastName: "",
     phone: ""
   };
+
+  products: Product[] = [
+    {
+      id: "1",
+      name: "product 1",
+      price: 100,
+      description: "description 1",
+      image: "https://m.media-amazon.com/images/I/71UOxU0t4-L._AC_SX466_.jpg",
+    },
+    {
+      id: "2",
+      name: "product 2",
+      price: 200,
+      description: "description 2",
+      image: "https://http2.mlstatic.com/jabon-liquido-natural-sin-ingredientes-daninos-D_NQ_NP_315011-MLM20452399840_102015-F.jpg",
+    },
+    {
+      id: "3",
+      name: "product 4",
+      price: 100,
+      description: "description 4",
+      image: "https://tse4.mm.bing.net/th?id=OIP.QTLafirwLtI6Od7ezQLZuQHaEv&pid=Api&P=0&h=180",
+    },
+    {
+      id: "4",
+      name: "product 4",
+      price: 100,
+      description: "description 4",
+      image: "https://3.bp.blogspot.com/-tWTsiaeKKzI/UPoVBH9uX4I/AAAAAAABhrs/tTvDQQlYhIU/s1600/helado-en-cono-de-sabor-chocolate-con-frambuezas-y-cerezas-ice-cream-cone.jpg",
+    }
+  ]
+
+//Funcionamiento bucle for of:
+// const numbers = [1,2,3,4,5];
+// const fruits = ["apple", "kiwi", "banana"];
+
+// for(const variable of numbers){
+//   console.log(variable)
+// }
+
+// const number = "";
+
+// if (number === 2){
+//   console.log("Este es un 2")
+// }else if(number === 3){
+//   console.log("Este es un 3")
+// }else if(number === 4){
+//   console.log("Este es un 4")
+// }else{
+//   console.log("No se que es")
+// }
+
+// switch(number){
+//   case 2:
+//     console.log("Este es un 2")
+//     break;
+//   case 3:
+//     console.log("Este es un 3")
+//     break
+//   case 4:
+//     console.log("Este es un 4")
+//     break
+//   default:
+//     console.log("No se que es")
+//     break;
+// }
 
   buttonClicked(){
     console.log("Hola mundo");
